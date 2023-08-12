82aef2a6373396ac1c0755906ee167969d093736
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-08-12 19:35:06 +0800

    Update test_save_reload_base_model.py

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+
+storage = FileStorage()
+storage.reload()
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
@@ -14,4 +17,3 @@ my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
 print(my_model)
-
