6359fbd92aa61dbefdc81147551714b29b2c5d86
InfintyJer <jclaris6@gmail.com>
2023-08-12 03:57:04 -0700

    tasks

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""BaseModel module"""
+from datetime import datetime
+import uuid
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key,
+                                datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of BaseModel instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
