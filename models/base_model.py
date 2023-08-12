3ae20341c23451d8b0a94a5dc63c6f503c5147fb
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-08-12 21:28:06 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,60 @@
 #!/usr/bin/python3
-"""BaseModel module"""
-import models
-from datetime import datetime
-import uuid
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+import uuid
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents a BaseModel Class
-    Args:
-        - *args: list of arguments
-        - **kwargs: dict of key-value pair arguments
-    """
+
+    """Class for base model of object hierarchy."""
 
     def __init__(self, *args, **kwargs):
-        """Initialisation Method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) == 0:
-            models.storage.new(self)
+        """Initialization of a Base instance.
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    if k in ['created_at', 'updated_at']:
-                        if type(v) == str:
-                            v = datetime.strptime(v, time)
-                        else:
-                            if k == 'created_at':
-                                v = self.created_at
-                            elif k == 'updated_at':
-                                v = self.updated_at
-                    if k == 'id':
-                        v = str(v)
-                    setattr(self, k, v)
-                if k == 'id':
-                    existing = models.storage.all()
-                    id_str = self.__class__.__name__ + '.' + str(v)
-                    if id_str not in existing.keys():
-                        models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """String representation of a class"""
-        cls_name = self.__class__.__name__
-        cls_dict = str(self.__dict__)
-        return "[{:s}] ({:s}) {:s}".format(cls_name, self.id, cls_dict)
+        """Returns a human-readable string representation
+        of an instance."""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute
-        updated_at with the current datetime
-        """
+        """Updates the updated_at attribute
+        with the current datetime."""
+
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing
-        all keys/values of __dict__ of the instance
-        """
-        dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name__
-        if "created_at" in dict_copy:
-            dict_copy['created_at'] = dict_copy['created_at'].isoformat()
-        if "updated_at" in dict_copy:
-            dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
-        return dict_copy
+        """Returns a dictionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
