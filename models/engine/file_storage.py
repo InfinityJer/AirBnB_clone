8ea5067f2020d4572fce0a6de89daf29fce96c49
Sahar <Aguidi53@gmail.com>
2023-08-13 09:06:08 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 """FileStorage module"""
 
-import datetime
-import models
 import json
-import os
-
-
-def models_encod_hook(obj):
-    """Returns an instance of a generated json string literal"""
-    try:
-        cls = obj['__class__']
-    except KeyError:
-        return obj
-    else:
-        try:
-            return getattr(models, cls)(**obj)
-        except AttributeError:
-            return obj
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
-    """serializes instances to a
-    JSON file & deserializes back
-    to instances"""
+    """contains an abstracted storage engine
 
-    # string - path to the JSON file (ex: file.json)
+    args:
+        __file_path (str): file name to save objects to
+        __objects (dict): A dictionary of instantiated objects
+    """
     __file_path = "file.json"
-    # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
     def all(self):
@@ -35,84 +26,25 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.__name__ + '.' + obj.id
-        FileStorage.__objects[key] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """convert to json"""
-        with open(self.__class__.__file_path, "w+") as f:
-            new_dict = {}
-            for key, value in self.__class__.__objects.items():
-                new_dict[key] = value.to_dict()
-            f.write(json.dumps(new_dict))
+        """Serialize __objects to the JSON file __file_path"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects if it exists"""
-        from_json = {}
+        """Deserialize the JSON file __file_path to __objects, if it exists"""
         try:
-            with open(self.__file_path, mode='r', encoding="UTF-8") as myfile:
-                from_json = json.load(myfile)
-                for key, value in from_json.items():
-                    attr_cls_name = value.pop("__class__")
-                    self.new(eval(attr_cls_name)(**value))
-        except Exception:
-            pass
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        class_dict = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Place': Place,
-            'Review': Review}
-        return class_dict
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                {"id": str,
-                 "created_at": datetime.datetime,
-                 "updated_at": datetime.datetime},
-            "User":
-                {"email": str,
-                 "password": str,
-                 "first_name": str,
-                 "last_name": str},
-            "State":
-                {"name": str},
-            "City":
-                {"state_id": str,
-                 "name": str},
-            "Amenity":
-                {"name": str},
-            "Place":
-                {"city_id": str,
-                 "user_id": str,
-                 "name": str,
-                 "description": str,
-                 "number_rooms": int,
-                 "number_bathrooms": int,
-                 "max_guest": int,
-                 "price_by_night": int,
-                 "latitude": float,
-                 "longitude": float,
-                 "amenity_ids": list},
-            "Review":
-                {"place_id": str,
-                 "user_id": str,
-                 "text": str}
-        }
-        return attributes
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
